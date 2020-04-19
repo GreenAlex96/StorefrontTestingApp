95512bbdb6f8bc4066e7df8c6887ac6f1e3abbc9
TheJayNite <patrickyodergd@gmail.com>
2020-04-19 17:53:57 -0400

    Code styling changes

--- home.php
+++ home.php
@@ -17,66 +17,63 @@
     include('dbConnect.php');
     ?>
 
-    <!-- Slideshow container -->
     <div class="slideshow">
+        <div class="slide fade">
+            <img src="./images/homepage1.png" style="width:100%">
+            <div class="captionText">Caption One</div>
+        </div>
 
-    <!-- Full-width images with number and caption text -->
-    <div class="slide fade">
-    <img src="./images/homepage1.png" style="width:100%">
-    <div class="captionText">Caption One</div>
-    </div>
+        <div class="slide fade">
+            <img src="./images/homepage2.png" style="width:100%">
+            <div class="captionText">Caption Two</div>
+        </div>
 
-    <div class="slide fade">
-    <img src="./images/homepage2.png" style="width:100%">
-    <div class="captionText">Caption Two</div>
-    </div>
+        <div class="slide fade">
+            <img src="./images/homepage3.png" style="width:100%">
+            <div class="captionText">Caption Three</div>
+        </div>
 
-    <div class="slide fade">
-    <img src="./images/homepage3.png" style="width:100%">
-    <div class="captionText">Caption Three</div>
-    </div>
-
-    <!-- Next and previous buttons -->
-    <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
-    <a class="next" onclick="changeSlide(1)">&#10095;</a>
+        <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
+        <a class="next" onclick="changeSlide(1)">&#10095;</a>
     </div>
     <br>
 
-    <!-- The dots/circles -->
     <div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="setSlide(1)"></span>
+        <span class="dot" onclick="setSlide(2)"></span>
+        <span class="dot" onclick="setSlide(3)"></span>
     </div>
 
     <script>
         var slideIndex = 1;
         showSlide(slideIndex);
 
-        // Next/previous controls
         function changeSlide(n) {
-        showSlide(slideIndex += n);
+            showSlide(slideIndex += n);
         }
 
-        // Thumbnail image controls
-        function currentSlide(n) {
-        showSlide(slideIndex = n);
+        function setSlide(n) {
+            showSlide(slideIndex = n);
         }
 
         function showSlide(n) {
-        var i;
-        var slides = document.getElementsByClassName("slide");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" slideActive", "");
-        }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " slideActive";
+            var slides = document.getElementsByClassName("slide");
+            var dots = document.getElementsByClassName("dot");
+            var i;
+            if (n > slides.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" slideActive", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " slideActive";
         }
     </script>
 </body>
